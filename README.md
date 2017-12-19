43afc7153d463cef2c4f68fcf3406b0b459f7bd1
福田康弘 <vac201@gmail.com>
2017-12-19 16:33:50 +0900

    初回コミット

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# wordpress-willmail-addusers
+# Wordpress "WiLL Mail" Add Users Plugin
 This is Wordpress plugin. This plugin will add users to Willmail.
+
+## ただいま作成中でっす！！／(^o^)＼
+
+### このプラグインは...
+
+WiLL Mail https://willcloud.jp/
+
+WiLL Mail さんは非常に優秀なメール送信のクラウドシステムです。
+developpers用にAPIも公開いただいています。
+
+このプラグインは、「Wordpressで作ったステップメールの登録フォームとWiLL Mailの顧客情報テーブルを連携したい！」という内容を実現するために制作しました。
+
+フォーム部分はcontactform 7 https://ja.wordpress.org/plugins/contact-form-7/ をご利用ください。
+
+### 設定方法
+
+1. 管理画面にログインし、当プラグインの設定画面を開いてください。指定されているAPIのURLを、ご登録ください。
+2. Contactform 7において、hiddenのフォームで、name=willmail value=Addusersとご指定ください。
+
+以上で連携されました！
+
+コンタクトフォームの入力画面にて、hiddenのフォームを作っていただくことで、WiLL Mailと連携することを意図することができます！
+そのため、必須チェック等は行いません！非常にユーザー任せのフォームになっています。
---
+++ wp-willmail-addusers.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * Plugin Name: WP Willmail Addusers
+ * Plugin URI:  https://github.com/1yaan/wp-willmail-addusers
+ * Description: This is Wordpress plugin. This plugin will add users to Willmail.
+ * Version:     0.1.0
+ * Author:      1yaan
+ * Author URI:  https://github.com/1yaan
+ * License:     GPLv2 or later
+ * License URI: https://www.gnu.org/licenses/gpl.html
+ * Text Domain: wp_willmail_addusers
+ *
+ * @link        https://github.com/1yaan/wp-willmail-addusers
+ * @since       0.1.0
+ * @package     wp-willmail-addusers
+ */
+
+/*
+Copyright 2017 1yaan
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License, version 2, as
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+// Make sure we don't expose any info if called directly.
+if ( ! function_exists( 'add_action' ) ) {
+	echo "Hi there! I'm just a plugin, not much I can do when called directly.";
+	exit;
+}

6537037f356e7a65538c23c8ec6cbfcfc0f1dda5
福田康弘 <vac201@gmail.com>
2017-12-22 10:58:52 +0900

    ActivatorとDeactivatorを追加

---
+++ includes/class-wp-willmail-addusers-activator.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      0.1.0
+ * @version    0.1
+ * @package    wp-bitcoin-chart
+ * @subpackage wp-bitcoin-chart/includes
+ * @author     1yaan, {@link https://github.com/1yaan https://github.com/1yaan}
+ * @copyright  1yaan, {@link https://github.com/1yaan https://github.com/1yaan}
+ * @license    GPLv2 or later, {@link https://www.gnu.org/licenses/gpl.html https://www.gnu.org/licenses/gpl.html}
+ */
+class WWA_Activator {
+
+	/**
+	 * Fired when the plugin is activated.
+	 *
+	 * @since    0.1.0
+	 */
+	public static function activate() {
+	} // end activate
+}
---
+++ includes/class-wp-willmail-addusers-deactivator.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      0.1.0
+ * @version    0.1
+ * @package    wp-bitcoin-chart
+ * @subpackage wp-bitcoin-chart/includes
+ * @author     1yaan, {@link https://github.com/1yaan https://github.com/1yaan}
+ * @copyright  1yaan, {@link https://github.com/1yaan https://github.com/1yaan}
+ * @license    GPLv2 or later, {@link https://www.gnu.org/licenses/gpl.html https://www.gnu.org/licenses/gpl.html}
+ */
+
+class WWA_Deactivator {
+
+	/**
+	 * Fired when the plugin is deactivated.
+	 *
+	 * @since    0.1.0
+	 */
+	public static function deactivate() {
+	} // end deactivate
+}

c4fa4d4d2fd7c95dff686ab22f139039c7d3b047
福田康弘 <vac201@gmail.com>
2017-12-22 16:15:12 +0900

    Unistallの追加

---
+++ uninstall.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Fired when the plugin is uninstalled.
+ *
+ * @since      0.1.0
+ * @version    0.1
+ * @package    wp-bitcoin-chart
+ * @subpackage wp-bitcoin-chart/includes
+ * @author     1yaan, {@link https://github.com/1yaan https://github.com/1yaan}
+ * @copyright  1yaan, {@link https://github.com/1yaan https://github.com/1yaan}
+ * @license    GPLv2 or later, {@link https://www.gnu.org/licenses/gpl.html https://www.gnu.org/licenses/gpl.html}
+ */
+
+// If uninstall, not called from WordPress, then exit
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit;
+}

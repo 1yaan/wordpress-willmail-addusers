415f0cba621ca65948177ef90f94f6c273eb37b8
福田康弘 <vac201@gmail.com>
2017-12-22 11:03:15 +0900

    コーディング規約違反修正 https://travis-ci.org/1yaan/wp-willmail-addusers/jobs/320025188

--- includes/class-wwa-deactivator.php
+++ includes/class-wwa-deactivator.php
@@ -12,7 +12,6 @@
  * @copyright  1yaan, {@link https://github.com/1yaan https://github.com/1yaan}
  * @license    GPLv2 or later, {@link https://www.gnu.org/licenses/gpl.html https://www.gnu.org/licenses/gpl.html}
  */
-
 class WWA_Deactivator {
 
 	/**
